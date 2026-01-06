5831d0f483bd198d349b30ab710ae0dfb7d22ca5
LingKuma <linghikki@gmail.com>
2026-01-06 22:48:53 +0800

    feat: 添加OCR测试脚本和示例图片
    添加测试脚本test.py用于PaddleOCR文本检测，包含两个示例图片用于测试

---
+++ test.py
@@ -0,0 +1,7 @@
+from paddleocr import TextDetection
+model = TextDetection(model_name="PP-OCRv5_server_det")
+output = model.predict("/", batch_size=1)
+for res in output:
+    res.print()
+    res.save_to_img(save_path="./output/")
+    res.save_to_json(save_path="./output/res.json")
