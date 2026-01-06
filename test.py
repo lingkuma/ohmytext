f9a55bdea4020a38fdd8fd9d901bbfe6669f853f
LingKuma <linghikki@gmail.com>
2026-01-07 02:44:37 +0800

    1

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from paddleocr import TextDetection
 model = TextDetection(model_name="PP-OCRv5_server_det")
-output = model.predict("/", batch_size=1)
+output = model.predict("./test_img/test1.png", batch_size=1)
 for res in output:
     res.print()
     res.save_to_img(save_path="./output/")
