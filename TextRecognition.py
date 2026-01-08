f9d064f4286b6ec924eb9889d040d7a7ac39e61a
LingKuma <linghikki@gmail.com>
2026-01-08 23:15:22 +0800

    1

--- TextRecognition.py
+++ TextRecognition.py
@@ -1,5 +1,5 @@
 from paddleocr import TextRecognition
-model = TextRecognition(model_name="PP-OCRv5_server_rec")
+model = TextRecognition(model_name="latin_PP-OCRv5_mobile_rec")
 output = model.predict(input="./test_img/general_ocr_rec_001.png", batch_size=1)
 for res in output:
     res.print()
